2906d1a4af593edf65e747e2ad5b186c770ba6b7
kevinv <kevinv@centrumsystems.com.au>
2011-04-07 14:27:59 +1000

    Removed the test testProjectConfigurePage as it assumed that the build-pipe-plugin was installed in an instance of Hudson that HudsonTestCase was running.

--- src/test/java/au/com/centrumsystems/hudson/plugin/buildpipeline/trigger/BuildPipelineTriggerTest.java
+++ src/test/java/au/com/centrumsystems/hudson/plugin/buildpipeline/trigger/BuildPipelineTriggerTest.java
@@ -34,14 +34,11 @@ import java.io.IOException;
 import java.util.List;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.jvnet.hudson.test.HudsonTestCase;
 import org.xml.sax.SAXException;
 
-import com.gargoylesoftware.htmlunit.ElementNotFoundException;
 import com.gargoylesoftware.htmlunit.html.HtmlButton;
-import com.gargoylesoftware.htmlunit.html.HtmlCheckBoxInput;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlRadioButtonInput;
@@ -64,7 +61,6 @@ public class BuildPipelineTriggerTest extends HudsonTestCase {
         super.setUp();
     }
 
-    @Ignore
     @Test
     public void testBuildPipelineTrigger() throws IOException {
         String proj1 = "Proj1";
@@ -80,48 +76,6 @@ public class BuildPipelineTriggerTest extends HudsonTestCase {
         assertNotNull("A valid BuildPipelineTrigger should have been created.", myBPTrigger);
 
         assertEquals("BuildPipelineTrigger downstream project is " + proj1, proj1, myBPTrigger.getDownstreamProjectNames());
-    }
-
-    @Test
-    public void testProjectConfigurePage() throws IOException, SAXException, ElementNotFoundException {
-        String proj1 = "Proj1";
-        String proj2 = "Proj2";
-        String proj1Href = ".." + "/job/" + proj1;
-        String proj2Href = ".." + "/job/" + proj2;
-
-        // Create two new projects
-        final HtmlPage project1ConfigurePage = createNewFreeStyleProjectHtmlPage(proj1);
-        final HtmlPage project2ConfigurePage = createNewFreeStyleProjectHtmlPage(proj2);
-
-        // Test if the HTML pages were successfully returned
-        assertNotNull("Create New Upstream Job Form Return Page failed", project1ConfigurePage);
-        assertNotNull("Create New Downstream Job Form Return Page failed", project2ConfigurePage);
-
-        // Save the configuration of the Downstream project
-        final HtmlForm configureNewProject2Form = project1ConfigurePage.getFormByName("config");
-        final HtmlButton saveNewProject2Button = (HtmlButton) last(configureNewProject2Form.getHtmlElementsByTagName("button"));
-        saveNewProject2Button.removeAttribute("disabled");
-        configureNewProject2Form.submit(saveNewProject2Button);
-
-        // Select the Build-Pipeline-Plugin radio button
-        final HtmlForm configureNewProject1Form = project2ConfigurePage.getFormByName("config");
-        final HtmlCheckBoxInput buildPipelinePluginCheckBox = project2ConfigurePage.getElementByName(BUILD_PIPELINE_PLUGIN_NAME);
-        buildPipelinePluginCheckBox.setChecked(true);
-        buildPipelinePluginCheckBox.click();
-        final HtmlTextInput buildPipelinePluginText = project1ConfigurePage.getElementByName("downstreamProjectNames");
-        buildPipelinePluginText.setValueAttribute(proj2);
-
-        // Set the Downstream Project Name
-        final HtmlButton saveNewProject1Button = (HtmlButton) last(configureNewProject1Form.getHtmlElementsByTagName("button"));
-        saveNewProject1Button.removeAttribute("disabled");
-        // configureNewProject1Form.submit(saveNewProject1Button);
-        final HtmlPage project1FinalPage = saveNewProject1Button.click();
-
-        // Retrieve the configuration page for the Upstream project and test that the correct downstream project has been added
-        // final HtmlPage project1FinalPage = new WebClient().goTo(proj1Href);
-        assertEquals("The downstream project should have been " + proj2Href, proj2Href, project1FinalPage.getAnchorByHref(proj2Href)
-                .getHrefAttribute());
-
     }
 
     @Test
