087600a4705560706da7f0cb3159504f2152aa68
kevinv <kevinv@centrumsystems.com.au>
2011-04-07 12:01:41 +1000

    Cleaned up tests.

--- src/test/java/au/com/centrumsystems/hudson/plugin/util/BuildUtilTest.java
+++ src/test/java/au/com/centrumsystems/hudson/plugin/util/BuildUtilTest.java
@@ -38,62 +38,53 @@ import org.jvnet.hudson.test.MockBuilder;
 
 public class BuildUtilTest extends HudsonTestCase {
 
-    private static final String TEST_PROJECT1 = "Main Project1";
-    private static final String TEST_PROJECT2 = "Downstream Project2";
-    private static final String TEST_PROJECT3 = "Downstream Project3";
-
     @Override
     @Before
-    public void setUp() {
-        try {
-            super.setUp();
-        } catch (Exception e) {
-            e.toString();
-        }
+    public void setUp() throws Exception {
+        super.setUp();
     }
 
     @Test
-    public void testGetDownstreamBuild() {
+    public void testGetDownstreamBuild() throws Exception {
+        String proj1 = "Proj1";
+        String proj2 = "Proj2";
+        String proj3 = "Proj3";
+
         FreeStyleProject project1, project2, project3;
         MockBuilder builder1, builder2, builder3;
         FreeStyleBuild build1, build2, build3;
 
         // Create test projects and associated builders
-        try {
-            project1 = createFreeStyleProject(TEST_PROJECT1);
-            builder1 = new MockBuilder(Result.SUCCESS);
-            project2 = createFreeStyleProject(TEST_PROJECT2);
-            builder2 = new MockBuilder(Result.SUCCESS);
-            project3 = createFreeStyleProject(TEST_PROJECT3);
-            builder3 = new MockBuilder(Result.SUCCESS);
+        project1 = createFreeStyleProject(proj1);
+        project2 = createFreeStyleProject(proj2);
+        builder2 = new MockBuilder(Result.SUCCESS);
+        project3 = createFreeStyleProject(proj3);
+        builder3 = new MockBuilder(Result.SUCCESS);
 
-            // Add project2 as a post build action: build other project
-            project1.getPublishersList().add(new BuildTrigger(TEST_PROJECT2, true));
-            project2.getPublishersList().add(new BuildTrigger(TEST_PROJECT3, true));
+        // Add project2 as a post build action: build other project
+        project1.getPublishersList().add(new BuildTrigger(proj2, true));
+        project2.getPublishersList().add(new BuildTrigger(proj3, true));
 
-            // Important; we must do this step to ensure that the dependency graphs are updated
-            Hudson.getInstance().rebuildDependencyGraph();
+        // Important; we must do this step to ensure that the dependency graphs are updated
+        Hudson.getInstance().rebuildDependencyGraph();
 
-            // Add the builders to the respective project's builder lists
-            project1.getBuildersList().add(builder1);
-            project2.getBuildersList().add(builder2);
-            project3.getBuildersList().add(builder3);
+        // Add the builders to the respective project's builder lists
+        project1.getBuildersList().add(builder1);
+        project2.getBuildersList().add(builder2);
+        project3.getBuildersList().add(builder3);
 
-            // Build project1, upon completion project2 will be built
+        // Build project1, upon completion project2 will be built
 
-            build1 = buildAndAssertSuccess(project1);
-            // When all building is complete retrieve the last build from project2
-            waitUntilNoActivity();
-            build2 = project2.getLastBuild();
-            build3 = project3.getLastBuild();
+        build1 = buildAndAssertSuccess(project1);
+        // When all building is complete retrieve the last build from project2
+        waitUntilNoActivity();
+        build2 = project2.getLastBuild();
+        build3 = project3.getLastBuild();
 
-            AbstractBuild<?, ?> nextBuild = BuildUtil.getDownstreamBuild(project2, build1);
-            assertEquals("The next build should be " + TEST_PROJECT1 + build2.number, build2, nextBuild);
+        AbstractBuild<?, ?> nextBuild = BuildUtil.getDownstreamBuild(project2, build1);
+        assertEquals("The next build should be " + proj1 + build2.number, build2, nextBuild);
 
-            nextBuild = BuildUtil.getDownstreamBuild(project3, nextBuild);
-            assertEquals("The next build should be " + TEST_PROJECT1 + build3.number, build3, nextBuild);
-        } catch (Exception e) {
-            e.toString();
-        }
+        nextBuild = BuildUtil.getDownstreamBuild(project3, nextBuild);
+        assertEquals("The next build should be " + proj1 + build3.number, build3, nextBuild);
     }
 }
