9e4e94e5b5df7675d7cc48762ec2dd83f3b31b3d
Marcin Popielarz <marcinp@centrumsystems.com.au>
2012-06-22 14:13:02 +1000

    changed refresh to 5 sec

--- src/main/java/au/com/centrumsystems/hudson/plugin/buildpipeline/BuildPipelineView.java
+++ src/main/java/au/com/centrumsystems/hudson/plugin/buildpipeline/BuildPipelineView.java
@@ -88,7 +88,7 @@ public class BuildPipelineView extends View {
     /**
      * Frequency at which the Build Pipeline Plugin updates the build cards in seconds
      */
-    private int refreshFrequency = 2;
+    private int refreshFrequency = 5;
 
     /*
      * Keep feature flag properties in one place so that it is easy to refactor them out later.
