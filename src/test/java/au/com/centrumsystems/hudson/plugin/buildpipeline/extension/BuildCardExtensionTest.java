98f39817e0651fee271febc6e12530d9bffe720b
James Forsee <jamesforsee@pingidentity.com>
2017-10-20 17:11:38 -0600

    Remove bad file license headers

--- src/test/java/au/com/centrumsystems/hudson/plugin/buildpipeline/extension/BuildCardExtensionTest.java
+++ src/test/java/au/com/centrumsystems/hudson/plugin/buildpipeline/extension/BuildCardExtensionTest.java
@@ -1,18 +1,27 @@
-/***************************************************************************
- * Copyright (C) 2014 Ping Identity Corporation
- * All rights reserved.
- * <p/>
- * The contents of this file are the property of Ping Identity Corporation.
- * You may not copy or use this file, in either source code or executable
- * form, except in compliance with terms set by Ping Identity Corporation.
- * For further information please contact:
- * <p/>
- * Ping Identity Corporation
- * 1001 17th Street Suite 100
- * Denver, CO 80202
- * 303.468.2900
- * http://www.pingidentity.com
- **************************************************************************/
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2016 the Jenkins project
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
 package au.com.centrumsystems.hudson.plugin.buildpipeline.extension;
 
 import au.com.centrumsystems.hudson.plugin.buildpipeline.BuildPipelineView;
