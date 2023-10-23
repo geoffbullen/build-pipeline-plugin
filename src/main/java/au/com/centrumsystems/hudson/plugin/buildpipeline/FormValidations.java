38714c11b2ffac64fe5fec309838294fb8af0c6a
Mark Waite <mark.earl.waite@gmail.com>
2023-10-22 19:34:19 -0600

    Resolve checkstyle complaints in FormValidations
    The checkstyle complaints are not very helpful, but it is easier to
    resolve the complaints than to declare some form of exception to the
    complaints.

--- src/main/java/au/com/centrumsystems/hudson/plugin/buildpipeline/FormValidations.java
+++ src/main/java/au/com/centrumsystems/hudson/plugin/buildpipeline/FormValidations.java
@@ -2,25 +2,40 @@ package au.com.centrumsystems.hudson.plugin.buildpipeline;
 
 import hudson.util.FormValidation;
 
-public class FormValidations
-{
-	private FormValidations()
-	{
-	}
+/**
+ * Form validation class for validation errors.
+ * @author Mathieu Mitchell
+ */
+public final class FormValidations {
+    /** Intentionally private constructor. */
+    private FormValidations() {
+    }
 
-	public static FormValidation noProjectSpecified()
-	{
-		return FormValidation.error("No project specified");
-	}
+    /**
+     * Returns form validation error when no project is specified.
+     * @return form validation error when no project is specified.
+     */
+    public static FormValidation noProjectSpecified() {
+        return FormValidation.error("No project specified");
+    }
 
-	public static FormValidation notBuildable(String projectName)
-	{
-		return FormValidation.error("‘" + projectName + "‘ not buildable");
-	}
+    /**
+     * Returns form validation error when project is not buildable.
+     * @param projectName name of project that is not buildable
+     * @return form validation error when project is not buildable.
+     */
+    public static FormValidation notBuildable(String projectName) {
+        return FormValidation.error("‘" + projectName + "‘ not buildable");
+    }
 
-	public static FormValidation noSuchProject(String projectName, String nearestProjectName)
-	{
-		return FormValidation.error(
-			"No such project ‘" + projectName + "‘. Did you mean ‘" + nearestProjectName + "‘?");
-	}
+    /**
+     * Returns form validation error when project does not exist.
+     * @param projectName name of project that does not exist
+     * @param nearestProjectName name of a project that might be near the non-existent project
+     * @return form validation error when project does not exist.
+     */
+    public static FormValidation noSuchProject(String projectName, String nearestProjectName) {
+        return FormValidation.error(
+                "No such project ‘" + projectName + "‘. Did you mean ‘" + nearestProjectName + "‘?");
+    }
 }
